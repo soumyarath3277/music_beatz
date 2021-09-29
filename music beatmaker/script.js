7580acd238eeef9885e996fafc6adc6fa2451a3a
soumyarath3277 <78521535+soumyarath3277@users.noreply.github.com>
2021-09-29 10:43:16 +0530

    Add files via upload

---
+++ music beatmaker/script.js
@@ -0,0 +1,37 @@
+window.addEventListener('load', () => {
+    const sounds = document.querySelectorAll(".sound");
+    const pads = document.querySelectorAll(".pads div");
+    const visual = document.querySelector('.visual');
+    const color = [
+        "#60d394",
+        "#d26060",
+        "#c060d3",
+        "#d3d160",
+        "#6860d3",
+        "#60d394"
+    ];
+    console.log(sounds[0]);
+
+    
+    //sounds
+    pads.forEach(pad, index) =>{
+        pad.addEventListener("click", function(){
+           sounds[index].currentTime=0;
+           sounds[index].play();
+
+           createBubbles(index);
+        }); 
+    });
+
+   //create a function that makes bubbles
+   const createBubbles= (index) => {
+       const bubble = document.createElement("div");
+       visual.appendChild(bubble); 
+       bubble.style.backgroundColor = colors[index];
+       bubble.style.animation="jump is ease";
+       bubble.addEventListener('animationed',function(){
+           visual.removeChild(this);
+
+       });
+   };
+});
